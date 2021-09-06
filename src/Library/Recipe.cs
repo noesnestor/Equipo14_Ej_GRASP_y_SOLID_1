4fc20db55bea3b24ed95861b7e09077cce1b18b4
Rodrigo Luque <rodrigoluquepuig@gmail.com>
2021-09-06 14:46:56 -0300

    Agregada funcionalidad de obtener el costo de produccion

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -15,6 +15,27 @@ namespace Full_GRASP_And_SOLID.Library
 
         public Product FinalProduct { get; set; }
 
+        /*
+        Utilizamos Expert para delegar la funcion de imprimir el costo total a
+        la clase Recipe. Debido a que debe imprimir un resultado en base a valores
+        que existen en otras clases, utilizando SRP delegamos el calculo del subtotal
+        a las clases que si conocen esa informacion.
+        */
+        public double GetProductionCost()
+        {
+            double result = 0;
+            double resultUnitCost = 0;
+            double resultEquipmentCost = 0;
+            foreach (Step item in this.steps)
+            {
+                resultUnitCost += item.UnitCostSubTotal;
+                resultEquipmentCost += item.EquipmentSubTotal;
+            }
+
+            result = resultUnitCost + resultEquipmentCost;
+            return Math.Round(result, 1);
+        }
+
         public void AddStep(Step step)
         {
             this.steps.Add(step);
@@ -33,6 +54,7 @@ namespace Full_GRASP_And_SOLID.Library
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
             }
+            Console.WriteLine($"Coste total de produccion: {this.GetProductionCost()}");
         }
     }
 }
